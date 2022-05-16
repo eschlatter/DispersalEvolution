12700f1c5d86bae561156bfc45c0beef15ead12a
eschlatter <eschlatter1@gmail.com>
2022-05-16 15:28:51 -0400

    Reworked IBM (using dists array) is working in scratch.m. Need to transfer it back to the main file.

--- files_runsims/test_RunIBM.m
+++ files_runsims/test_RunIBM.m
@@ -11,11 +11,11 @@ clear all; clc; close all
 gflag = 0;     % whether (1) or not (0) to show plots during simulation
 b = 10;        % offspring produced per individual
 del = 0.001;   % fraction of dispersal probability to move during mutation
-pvec = 1;      % probability of surviving dispersal
+p = 1;      % probability of surviving dispersal
 
 %ENVIRONMENT PARAMETERS
-nbins_env = 20; % max number of dispersal bins used when creating env (>=2)
-nbins = 20; % how many dispersal bins to actually use (2<=nbins_use<=nbins_max)
+nbins_env = 5; % max number of dispersal bins used when creating env (>=2)
+nbins = 5; % how many dispersal bins to actually use (2<=nbins_use<=nbins_max)
 eflag = 2; % which environment to use: 1=unbounded, 2=bounded, 3=reef
 S = 32^2;      % number of sites in the environment
 sx = 2; % number of sites in the x-dimension of the environment
@@ -24,6 +24,6 @@ sy = S/sx;
 G = 5; % number of total generations to simulate
 
 % larval navigation distance of 0, 1, 2 or 3
-nmax = 0;  % maximum larval recruitment distance (behavior)
+nmax = 1;  % maximum larval recruitment distance (behavior)
 
-IBM_dispersal_2D(gflag,eflag,sx,sy,nbins_env,nbins,nmax,G,b,pvec,del)
+test_dists_IBM_dispersal_2D(gflag,eflag,sx,sy,nbins_env,nbins,nmax,G,b,p,del)
